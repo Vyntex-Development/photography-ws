66ead1cbfd55e209bb07ff212bec50fab8726d70
adamovic6666 <adamovicslobodan@yahoo.com>
2022-08-24 17:29:06 +0200

    added new busines plan section

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Footer from "../components/layout/Footer";
 import Header from "../components/layout/Header";
+import BusinessPlan from "../components/pages/Homepage/BusinessPlan";
 
 const home = () => {
   return (
     <div>
       <Header />
+      <BusinessPlan />
       <Footer />
     </div>
   );
