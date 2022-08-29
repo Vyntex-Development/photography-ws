a8addaa0eaab3d7bf6362a61da570800b6981aa9
Paulie <matija@vyntex.com>
2022-08-29 17:33:08 +0200

    about page

---
+++ components/pages/About/About.js
@@ -0,0 +1,38 @@
+import classes from "./About.module.css";
+import Image from "next/image";
+import Link from "next/link";
+
+const About = () => {
+
+    return (
+        <div className={classes.AboutHeaderWrapper}>
+            <div className={`${classes.LinksWrapper} container`}> 
+                <Link passHref={true} href="https://www.facebook.com/">
+                    <a className={classes.Link}>FACEBOOK</a>
+                </Link>
+                <Link passHref={true} href="https://www.twitter.com/">
+                    <a className={classes.Link}>TWITTER</a>
+                </Link>
+                <Link passHref={true} href="https://www.instagram.com/">
+                    <a className={classes.Link}>INSTAGRAM</a>
+                </Link>
+                <Link passHref={true} href="https://www.behance.net/">
+                    <a className={classes.Link}>BEHANCE</a>
+                </Link>
+            </div>
+             <div className={classes.AboutHeader}>
+                <Image
+                layout="fill"
+                objectFit="cover"
+                alt="header"
+                src="/images/header.png"
+            ></Image>
+
+            </div>
+        </div>
+       
+    )
+
+}
+
+export default About;
---
+++ components/pages/About/Who.js
@@ -0,0 +1,40 @@
+import classes from "./Who.module.css";
+import Image from "next/image";
+import Link from "next/link";
+
+const Who = () => {
+
+    return (
+        <div className="container">
+            <div className={classes.WhoWrapper}>
+                <div className={classes.HeadingParagraphWrapper}>
+                    <h1>Who we are</h1>
+                    <p>In Pedro Almodovar’s  early film ¿Qué he hecho yo para merecer esto?  What  Have I Done to Deserve this? - two writers devise a plan to sell  diaries, although they know them to be forged. Their plot depends upon  the protagonist's husband Antonio</p>
+                </div>
+                <div className={classes.ImageWrapper}>
+                    <Image
+                    layout="fill"
+                    objectFit="cover"
+                    alt="who"
+                    src="/images/who.png"
+                ></Image>
+                </div>
+
+                <div className={classes.Down}>
+                    <div className={classes.Left}></div>
+                    <div className={classes.Right}>
+                        <h2>Our Story</h2>
+                        <p>As such, he is convinced that his work is really not forgery, since the source material – or so he was lead to believe – is true. Certainly  documents  and  records  can  serve  as  some measure of authenticity, but the events, experiences and self­understanding of the autobiographical subject can only be confirmed by the subject herself; the signa­ture  serves  as  a  guarantor.</p>
+                        <p>As madcap a storyline as this might seem, it illustrates a problem that has always plagued autobi­ography and its claims to veracity: only the signature of the autobiographical subject attests  to  the  truth  of  what  follows.</p>
+                        <p>As Almodovar’s forger reveals, the signature has limited probative value; it may simply be a lie. In an age when major publishing houses rely on a battery of ghost writers, the signature often indicates that the apparent autobio­graphical subject has merely signed off on what appears under her name but cannot claim authorship strictly speaking.</p>
+                    </div>
+                </div>
+            </div>
+           
+        </div>
+       
+    )
+
+}
+
+export default Who;
