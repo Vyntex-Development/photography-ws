ee24c5bc156b82aade576ea20ddd32c0efbeabe9
Paulie <matija@vyntex.com>
2022-08-30 15:23:48 +0200

    blog hero

--- components/pages/Blog/BlogPage.js
+++ components/pages/Blog/BlogPage.js
@@ -1,15 +1,49 @@
-import Subscribe from "./Subscribe";
-import Articles from "./Articles";
-import MostTrending from "./MostTrending";
+import classes from "./BlogPage.module.css";
+import Image from "next/image";
+import Link from "next/link";
+
 
 const BlogPage = () => {
-  return (
-    <>
-      <MostTrending />
-      <Articles />
-      <Subscribe />
-    </>
-  );
+    return (
+
+    <div className="container">
+    <div className={classes.HeroWrapper}>
+        <div className={classes.ArrowDotWrapper}>
+        <Link passHref={true} href="https://www.google.com/">
+            <a>
+            <div className={classes.ArrowWrapper}>
+                <Image
+                    layout="fill"
+                    objectFit="cover"
+                    alt="blog-arrow"
+                    src="/images/blog-arrow.png"
+                ></Image>
+
+            </div>
+            </a>
+        </Link>
+           
+            <span className={classes.Dot}></span>
+        </div>
+        
+        <h2>Best Camera for Portrait Photography</h2>
+        <p>Killer classics behind the scenes Kate Moss does Anna Wintour smile fall fashion hashtag.</p>
+        
+                <div className={classes.ImageWrapper}>
+                    <Image
+                        layout="fill"
+                        objectFit="cover"
+                        alt="blog-hero"
+                        src="/images/blog-hero.png"
+                    ></Image>
+                </div>
+        
+       
+    </div>
+</div>
+    )
+
+
 };
 
 export default BlogPage;
---
+++ pages/blogs/blog.js
@@ -0,0 +1,13 @@
+import BlogPage from "../../components/pages/Blog/BlogPage";
+
+const blog = () => {
+    return (
+        
+        <>
+        <BlogPage />
+        </>
+    )
+
+}
+
+export default blog;
